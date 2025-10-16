31dedfcab48ecc77d170342469439d72318b08d5
zhang-gh-123 <newssister@gmail.com>
2025-10-16 19:11:46 +0900

    Add files via upload

--- text.cob
+++ text.cob
@@ -1,5 +1,20 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLOGITHUB.
+       PROGRAM-ID. ADDNUMBERS.
+
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       01 NUM1       PIC 9(3).
+       01 NUM2       PIC 9(3).
+       01 SUM        PIC 9(4).
+
        PROCEDURE DIVISION.
-           DISPLAY "Hello, GitHub!".
+           DISPLAY "Enter first number: ".
+           ACCEPT NUM1
+           
+           DISPLAY "Enter second number: ".
+           ACCEPT NUM2
+           
+           COMPUTE SUM = NUM1 + NUM2
+           
+           DISPLAY "The sum is: " SUM
            STOP RUN.
